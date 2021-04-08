74f920dfda6f07dfc0249caf443016165d6dffe2
Jesse Redfield <jesse.redfield@tutanota.com>
2021-04-07 22:01:52 -0400

    remove the OSX demo we don't need anymore since console app works, and fix windows script

--- notificationicon_native/build.bat
+++ notificationicon_native/build.bat
@@ -1,16 +1,21 @@
 @echo off
 
 :: Setup paths to project paths
-set "BUILD_ROOT=%~dp0\_build\windows"
+set "BUILD_ROOT=%~dp0\_build\win"
 set "BUILD_X64=%BUILD_ROOT%\x64"
 if EXIST "%BUILD_X64%" rmdir /s /q "%BUILD_X64%"
 md "%BUILD_X64%"
 
 :: Setup paths to project outputs
-set "BIN_ROOT=%~dp0\bin\windows"
+set "BIN_ROOT=%~dp0\bin\win"
 set "BIN_X64=%BIN_ROOT%\x64"
 if EXIST "%BIN_X64%" rmdir /s /q "%BIN_X64%"
 md "%BIN_X64%"
+
+:: Setup paths to project outputs
+set "BIN_PREBUILD=%~dp0\..\prebuilt\win"
+if EXIST "%BIN_PREBUILD%" rmdir /s /q "%BIN_PREBUILD%"
+md "%BIN_PREBUILD%"
 
 :: Generate x64 build files
 pushd "%BUILD_X64%"
@@ -35,8 +40,7 @@ if %errorlevel% neq 0 goto :eof
 popd
 
 :: Copy artifacts
-set "PREBUILT_ROOT=%~dp0\..\..\prebuilt\win64"
-copy /Y "%BIN_X64%\Debug\native_tts.dll" "%PREBUILT_ROOT%\Debug\"
-copy /Y "%BIN_X64%\Debug\native_tts.pdb" "%PREBUILT_ROOT%\Debug\"
-copy /Y "%BIN_X64%\Release\native_tts.dll" "%PREBUILT_ROOT%\Release\"
+set "PREBUILT_ROOT=%~dp0"
+xcopy /Y "%BIN_X64%\Debug" "%BIN_PREBUILD%\x64\Debug\"
+xcopy /Y "%BIN_X64%\Release" "%BIN_PREBUILD%\x64\Release\"
 goto :eof
